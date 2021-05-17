09ed362e53a11538534e833b19f820610a2d3e74
DarkKnight <pks9862728888@gmail.com>
2021-05-17 14:36:38 +0530

    initial commit

---
+++ src/test/java/com/self/covaxinavailibilityselfalert/CovaxinAvailibilitySelfAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self.covaxinavailibilityselfalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovaxinAvailibilitySelfAlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
